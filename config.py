b0a7dac1cb6b8e5f3bbc97b0aa559ea36f665a4d
Mangouste <antoine.compagnie@gmail.com>
2021-06-24 14:09:42 +0200

    Delete config

--- config.py
+++
@@ -1,6 +0,0 @@
-url = 'mysql://bb9f0d500c90e4:1b33575e@eu-cdbr-west-01.cleardb.com/heroku_02c821a76ca04c8'
-
-MYSQL_DATABASE_USER = 'bb9f0d500c90e4'
-MYSQL_DATABASE_PASSWORD = '1b33575e'
-MYSQL_DATABASE_DB = 'heroku_02c821a76ca04c8'
-MYSQL_DATABASE_HOST = 'eu-cdbr-west-01.cleardb.com'
